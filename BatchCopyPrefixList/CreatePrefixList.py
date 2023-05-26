d5f15b4eada2e6c7b9f8aee5ddc60801a036b2ac
benxiwan <benxiwan@amazon.com>
2023-05-26 15:54:44 +0800

    fix os bug

--- BatchCopyPrefixList/CreatePrefixList.py
+++ BatchCopyPrefixList/CreatePrefixList.py
@@ -13,7 +13,7 @@ def script_handler(region, path, max_entries):
   for filename in dir_list:
     if filename.endswith(".csv"):
       #print(filename)
-      csvfile = open(path + '/' + filename, 'r')
+      csvfile = open(os.path.join(path, filename))
       lines = csvfile.readlines()
       entries = []
       count = 0
