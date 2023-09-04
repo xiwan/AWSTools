beb03558e6bf32ae0b5142e6554907cf196358d0
wan.xi <5155280@qq.com>
2023-09-04 17:19:12 +0800

    fix cloudformation bugs

--- CDK/apigw2sagemaker/lambda/lambda-handler.py
+++ CDK/apigw2sagemaker/lambda/lambda-handler.py
@@ -2,10 +2,31 @@ import os
 import io
 import boto3
 import json
+import csv
 
 # grab environment variables
 ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+runtime= boto3.client('runtime.sagemaker')
 
-def handler(event, context):
-    print(ENDPOINT_NAME)
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    
+    inputsData = {'inputs': event}
+    data = json.dumps(inputsData)
+    payload = data
+    
+    # print("=================1")
+    # print(payload)
+    
+    response = runtime.invoke_endpoint(
+        EndpointName=ENDPOINT_NAME,
+        ContentType='application/json',
+        Accept = "application/json",
+        Body=payload)
+       
+    # print("=================2")
+    # print(response)
+    result = json.loads(response['Body'].read().decode())
+    print(result)
+    
     return 200
